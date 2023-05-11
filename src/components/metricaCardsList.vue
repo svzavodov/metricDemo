4c5cbe5b9c93922afefb719bc7bdc4bc140045ab
Sergey Zavodov <drack702@gmail.com>
2023-05-11 18:37:13 +0500

    Компонент metricaCardsList
    -добавил компонент для отрисовки списка

---
+++ src/components/metricaCardsList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cards-list flex flex-col gap-[20px]" >
+      <metrica-card v-for="card in props.cards.value" :key="card.ssrKey" :card="card" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import {useCards} from '@/hooks/useCards'
+import metricaCard from '@/components/metricaCard.vue';
+
+const props = defineProps( {
+  cards: Object,
+})
+console.log(props);
+</script>
+
+<style scoped>
+ 
+</style>
